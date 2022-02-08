d02f58d00aed27680def5e2c76dd6bda7f8d334f
LyubooShv <liubomirshivachev@gmai.com>
2022-02-08 11:15:01 +0200

    fixed comments and redirect functionality

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,18 +1,22 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import Button from "@mui/material/Button";
 import { useDispatch, useSelector } from "react-redux";
 import { selectCurrentUser } from "./user.selector";
 import { logOutSuccess } from "../../containers/LogIn/action";
+import { redirect } from "../CustomRedirect/actions";
 
 const Header = () => {
   const currentUser = useSelector(selectCurrentUser);
   const dispatch = useDispatch();
+
+  const logOut = () => {
+    dispatch(redirect("/login"));
+    dispatch(logOutSuccess());
+  };
   return (
     <div>
       <h2>Simple Cars</h2>
-      <Link to="/login">LogIn</Link>
-      <Button onClick={() => dispatch(logOutSuccess())}>LogOut</Button>
+      <Button onClick={logOut}>LogOut</Button>
       <div>{currentUser && currentUser.data.user.username}</div>
     </div>
   );
